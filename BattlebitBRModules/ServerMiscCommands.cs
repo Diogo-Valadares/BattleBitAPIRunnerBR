830482b38e722f230cac4e92202a29b1511d4c36
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2023-08-26 15:45:44 -0300

    server MiscCommands update

--- BattlebitBRModules/ServerMiscCommands.cs
+++ BattlebitBRModules/ServerMiscCommands.cs
@@ -14,6 +14,7 @@ namespace BattlebitBRModules
         public bool AllowNV { get; set; } = true;
         public bool KillFeed { get; set; } = true;
         public bool Bleeding { get; set; } = true;
+        public bool Stamina { get; set; } = false;
     }
 
     [RequireModule(typeof(CommandHandler))]
@@ -21,36 +22,58 @@ namespace BattlebitBRModules
     {
         public Configuration ServerMiscConfig { get; set; }
 
-        [CommandCallback("TogModifier", Description = "Toggles an server modifier on or off", AllowedRoles = Roles.Admin)]            
-        public void TogModifier(RunnerPlayer commandSource, string modifier, bool on)
+        [CommandCallback("TogItemOn", Description = "Toggles an server item on", AllowedRoles = Roles.Admin)]
+        public void TogItemOn(RunnerPlayer commandSource, string item)
         {
-            switch (modifier)
+            switch (item)
             {
                 case "nv":
                 case "nightvision":
-                    ServerMiscConfig.AllowNV = on;
+                    ServerMiscConfig.AllowNV = true;
+                    break;
+            }
+        }
+        [CommandCallback("TogItemOff", Description = "Toggles an server item on", AllowedRoles = Roles.Admin)]
+        public void TogItemOff(RunnerPlayer commandSource, string item)
+        {
+            switch (item)
+            {
+                case "nv":
+                case "nightvision":
+                    ServerMiscConfig.AllowNV = false;
                     break;
             }
         }
 
-        [CommandCallback("TogItem", Description = "Toggles an server modifier on or off", AllowedRoles = Roles.Admin)]
-        public void TogItem(RunnerPlayer commandSource, string modifier, bool on)
+        [CommandCallback("TogMechanic on", Description = "Toggles an server mechanic on or off", AllowedRoles = Roles.Admin)]
+        public void TogMechanicOn(RunnerPlayer commandSource, string mechanic)
         {
-            switch (modifier)
-            {
-            }
-        }
-
-        [CommandCallback("TogMechanic", Description = "Toggles an server modifier on or off", AllowedRoles = Roles.Admin)]
-        public void TogMechanic(RunnerPlayer commandSource, string modifier, bool on)
-        {
-            switch (modifier)
+            switch (mechanic)
             {
                 case "bleeding":
-                    ServerMiscConfig.Bleeding = on;
+                    ServerMiscConfig.Bleeding = true;
                     break;
                 case "killfeed":
-                    ServerMiscConfig.KillFeed = on;
+                    ServerMiscConfig.KillFeed = true;
+                    break;
+                case "stamina":
+                    ServerMiscConfig.Stamina = true;
+                    break;
+            }
+        }
+        [CommandCallback("TogMechanic off", Description = "Toggles an server mechanic on or off", AllowedRoles = Roles.Admin)]
+        public void TogMechanicOff(RunnerPlayer commandSource, string mechanic)
+        {
+            switch (mechanic)
+            {
+                case "bleeding":
+                    ServerMiscConfig.Bleeding = false;
+                    break;
+                case "killfeed":
+                    ServerMiscConfig.KillFeed = false;
+                    break;
+                case "stamina":
+                    ServerMiscConfig.Stamina = false;
                     break;
             }
         }
@@ -61,6 +84,7 @@ namespace BattlebitBRModules
             player.Modifications.KillFeed = ServerMiscConfig.KillFeed;
             if (ServerMiscConfig.Bleeding) player.Modifications.EnableBleeding();
             else player.Modifications.DisableBleeding();
+            player.Modifications.StaminaEnabled = ServerMiscConfig.Stamina;
             await Task.CompletedTask;
         }
     }
