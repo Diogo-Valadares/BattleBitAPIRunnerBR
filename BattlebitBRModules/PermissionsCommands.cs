9585823cd5a3bdb5211db75a6d67ac631a205c1e
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2023-08-26 14:58:16 -0300

    Copy base commands plugin

---
+++ BattlebitBRModules/PermissionsCommands.cs
@@ -0,0 +1,40 @@
+﻿using BattleBitAPI.Common;
+using BBRAPIModules;
+using Commands;
+using Permissions;
+
+namespace PermissionsManager;
+
+/// <summary>
+/// Author: @RainOrigami
+/// Version: 0.4.5.1
+/// </summary>
+
+[RequireModule(typeof(PlayerPermissions))]
+[RequireModule(typeof(CommandHandler))]
+public class PermissionsCommands : BattleBitModule
+{
+    [ModuleReference]
+    public PlayerPermissions PlayerPermissions { get; set; }
+    [ModuleReference]
+    public CommandHandler CommandHandler { get; set; }
+
+    public override void OnModulesLoaded()
+    {
+        this.CommandHandler.Register(this);
+    }
+
+    [CommandCallback("addperm", Description = "Adds a permission to a player", AllowedRoles = Roles.Admin)]
+    public void AddPermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
+    {
+        this.PlayerPermissions.AddPlayerRoles(player.SteamID, permission);
+        commandSource.Message($"Added permission {permission} to {player.Name}");
+    }
+
+    [CommandCallback("removeperm", Description = "Removes a permission from a player", AllowedRoles = Roles.Admin)]
+    public void RemovePermissionCommand(RunnerPlayer commandSource, RunnerPlayer player, Roles permission)
+    {
+        this.PlayerPermissions.RemovePlayerRoles(player.SteamID, permission);
+        commandSource.Message($"Removed permission {permission} from {player.Name}");
+    }
+}
