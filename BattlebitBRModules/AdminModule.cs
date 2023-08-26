a1c7e9c72ac3d00348b62ce64ea8850038a29530
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2023-08-26 12:18:59 -0300

    Teste inicial de comandos para desativar/ativar NV e killfeed

---
+++ BattlebitBRModules/AdminModule.cs
@@ -0,0 +1,73 @@
+﻿using BattleBitAPI.Common;
+using BBRAPIModules;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattlebitBRModules
+{
+    public class AdminModule : BattleBitModule
+    {
+        private readonly ulong[] moderatorsID = {
+            76561197987717578,//goim                                                  
+            76561198105203764//dx2
+            //adicionar mais aqui depois
+        };
+
+        private bool allowNV = true;
+        private bool killFeed = true;
+
+        public override async Task OnPlayerSpawned(RunnerPlayer player)
+        {
+            player.Modifications.CanUseNightVision = allowNV;
+            player.Modifications.KillFeed = killFeed;
+            await Task.CompletedTask;
+        }
+
+        public override async Task<bool> OnPlayerTypedMessage(RunnerPlayer player, ChatChannel channel, string msg)
+        {
+            if (!msg.StartsWith('!')) return await Task.FromResult(false);
+            var command = msg.Split(" ");
+
+            switch (command[0])
+            {
+                case "!item":
+                    if (moderatorsID.ToList().Contains(player.SteamID))
+                        ItemCommand(player, command);
+                    else
+                        Server.MessageToPlayer(player.SteamID, "Você não tem permissão para usar esse comando");
+                    break;
+                case "!option":
+                    if (moderatorsID.ToList().Contains(player.SteamID))
+                        OptionCommand(player, command);
+                    else
+                        Server.MessageToPlayer(player.SteamID, "Você não tem permissão para usar esse comando");
+                    break;       
+                case "!ping":
+                    Server.MessageToPlayer(player, "Pong!");
+                    break;
+            }
+            return await Task.FromResult(true);
+        }
+
+        private void ItemCommand(RunnerPlayer player, string[] command)
+        {
+            switch (command[1])
+            {
+                case "NV":
+                case "NightVision":
+                    allowNV = command[1] != "ban";
+                    break;
+            }
+        }
+
+        private void OptionCommand(RunnerPlayer player, string[] command)
+        {
+            switch (command[1])
+            {
+                case "KillFeed":
+                    killFeed = command[1] != "off";
+                    break;
+            }
+        }
+    }
+}
---
+++ BattlebitBRModules/KillMessages.cs
@@ -0,0 +1,33 @@
+﻿using BattleBitAPI;
+using BattleBitAPI.Common;
+using BBRAPIModules;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Classes;
+
+public class KillMessages : BattleBitModule
+{
+    public override async Task OnAPlayerDownedAnotherPlayer(OnPlayerKillArguments<RunnerPlayer> onPlayerKill)
+    {
+        if(onPlayerKill.KillerTool == Gadgets.SledgeHammer || onPlayerKill.KillerTool == Gadgets.Pickaxe)
+        {
+            Server.SayToAllChat(onPlayerKill.Killer.Name + " passou a lambida em " + onPlayerKill.Victim.Name);
+            await Task.CompletedTask;
+            return;
+        }        
+        
+        float distance = Vector3.Distance(onPlayerKill.VictimPosition, onPlayerKill.KillerPosition);
+
+        if (distance >= 100f)
+        {
+            Server.SayToAllChat(onPlayerKill.Killer.Name + " matou " + onPlayerKill.Victim.Name + " com uma distancia de: " + distance);
+            await Task.CompletedTask;
+        }
+        else
+        {
+            Server.SayToAllChat(onPlayerKill.Killer.Name + " matou em uma distancia inferior a 100 metros");
+            await Task.CompletedTask;
+        }
+    }
+}
